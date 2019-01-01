2c89163860b5ebfdea0db2b7c5c11e23b20c8b27
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2019-01-01 01:45:56 -0500

    Added numbering for sessions to pyde.sh

--- pyde.sh
+++ pyde.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 function pyde {
-    SESSION='ipy'
+    SBASE="pyde"
+    let NUM="$(tmux ls | grep $SBASE | tail -n 1 | cut -d ' ' -f 1 |\
+	       sed 's/[^0-9]*//g')"+1
+
+    SESSION="$SBASE$NUM"
     tmux new -s $SESSION -d
     tmux split-window -v
     
     # sleep to prevent commands being sent before window creation
     sleep 1.5
     
-    tmux send-keys 'ipython' C-m
+    tmux send-keys "ipython" C-m
     tmux select-pane -t 0
     if [ $# -eq 1 ]
     then 
-        tmux send-keys 'vim $1' C-m
+        tmux send-keys "vim $1" C-m
     else
-        tmux send-keys 'vim' C-m
+        tmux send-keys "vim" C-m
     fi
     tmux attach -t $SESSION
 }

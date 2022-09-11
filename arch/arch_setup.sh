9c6d79ca9630b2c6a6c57d5877c9aa1a8180610f
bredanfitz <brendanjfitzpatrick0@gmail.com>
2022-09-11 14:23:33 -0500

    Added picom

--- arch/arch_setup.sh
+++ arch/arch_setup.sh
@@ -1,5 +1,15 @@
+# zsh shell
 ln -s ~/Documents/GitHub/config-files/shell/.zshrc ~/ 
 ln -s ~/Documents/GitHub/config-files/shell/.p10k.zsh ~/ 
+
+# vim
 ln -s ~/Documents/GitHub/config-files/shell/.vimrc ~/ 
+
+# i3
 ln -s ~/Documents/GitHub/config-files/arch/i3/config ~/.config/i3
+
+# rofi
 ln -s ~/Documents/GitHub/config-files/arch/rofi/config.rasi ~/.config/rofi/
+
+# picom
+ln -s ~/Documents/GitHub/config-files/arch/picom/picom.conf ~/.config/picom

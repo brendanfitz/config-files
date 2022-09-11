ee43959373e9e8532dc1128248b875529e770383
bredanfitz <brendanjfitzpatrick0@gmail.com>
2022-09-11 14:09:22 -0500

    Added rofi to arch setup

--- arch/arch_setup.sh
+++ arch/arch_setup.sh
@@ -1,3 +1,5 @@
 ln -s ~/Documents/GitHub/config-files/shell/.zshrc ~/ 
 ln -s ~/Documents/GitHub/config-files/shell/.p10k.zsh ~/ 
 ln -s ~/Documents/GitHub/config-files/shell/.vimrc ~/ 
+ln -s ~/Documents/GitHub/config-files/arch/i3/config ~/.config/i3
+ln -s ~/Documents/GitHub/config-files/arch/rofi/config.rasi ~/.config/rofi/

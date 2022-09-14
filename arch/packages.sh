228856d41635445a12a2925a2cb82a654a618efb
brendanfitz <brendanjfitzpatrick0@gmail.com>
2022-09-14 18:15:39 -0500

    Added arch packages

---
+++ arch/packages.sh
@@ -0,0 +1,22 @@
+sudo pacman -S powerline-fonts
+sudo  pacman -S alacritty
+pacman -S ctags
+sudo pacman -S ctags
+pacman -S man
+sudo pacman -S man
+pacman -S speedtest-cli
+sudo pacman -S speedtest-cli
+sudo pacman -S i2cdetect
+sudo pacman -S i2c-tools
+sudo pacman -S lm_sensors
+pacman -S python-pip
+sudo pacman -S python-pip
+pacman -S jupyter-notebook
+sudo pacman -S jupyter-notebook
+sudo pacman -S pulseaudio
+sudo pacman -S pulseaudio-bluetooth
+sudo pacman -S bluez bluez-utils blueman
+sudo pacman -S pulseaudio-bluetooth
+sudo pacman -S pulseaudio
+pacman -S perl-anyevent-i3 perl-json-xs
+sudo pacman -S perl-anyevent-i3 perl-json-xs

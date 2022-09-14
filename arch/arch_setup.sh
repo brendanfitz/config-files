7b634537b44f6608882d8905a3206a728f732d24
brendanfitz <brendanjfitzpatrick0@gmail.com>
2022-09-14 18:52:31 -0500

    Fixing up arch setup

--- arch/arch_setup.sh
+++ arch/arch_setup.sh
@@ -1,44 +1,44 @@
 # alacritty
-rm ~/.xinitrc
+rm -f ~/.xinitrc
 ln -s ~/Documents/GitHub/config-files/arch/xinit/.xinitrc ~/
 
 # alacritty
-mkdir ~/.config/alacritty
-rm ~/.config/alacritty/alacritty.yml
-ln -s ~/Documents/GitHub/config-files/arch/alacritty/alacritty.yml ~/.config/alacritty/
+mkdir -p ~/.config/alacritty
+rm -f ~/.config/alacritty/alacritty.yml
+ln -s ~/Documents/GitHub/config-files/arch/shell/alacritty.yml ~/.config/alacritty/
 
 # zsh shell
-rm ~/.zshrc
-ln -s ~/Documents/GitHub/config-files/shell/.zshrc ~/ 
-rm ~/.p10k.zsh
-ln -s ~/Documents/GitHub/config-files/shell/.p10k.zsh ~/ 
+rm -f ~/.zshrc
+ln -s ~/Documents/GitHub/config-files/arch/shell/.zshrc ~/ 
+rm -f ~/.p10k.zsh
+ln -s ~/Documents/GitHub/config-files/arch/shell/.p10k.zsh ~/ 
 
 # vim
-rm /etc/vimrc
-ln -s ~/Documents/GitHub/config-files/shell/.vimrc /etc/vimrc
+sudo rm -f /etc/vimrc
+sudo ln -s ~/Documents/GitHub/config-files/arch/shell/.vimrc /etc/vimrc
 
 # i3
-mkdir ~/.config/i3
-rm ~/.config/i3/config
-ln -s ~/Documents/GitHub/config-files/arch/i3/config ~/.config/i3/
+mkdir -p ~/.config/i3
+rm -f ~/.config/i3/config
+n -s ~/Documents/GitHub/config-files/arch/i3/config ~/.config/i3/
 
-rm ~/.config/i3/wallpaper.jpeg
-# wget -O ~/.config/i3/wallpaper.jpeg https://i.imgur.com/pMnZ1pT.jpeg
+rm -f ~/.config/i3/wallpaper.jpeg
+wget -O ~/.config/i3/wallpaper.jpeg https://i.imgur.com/pMnZ1pT.jpeg
 
 # rofi
-mkdir ~/.config/rofi
-rm ~/.config/rofi/config.rasi
+mkdir -p ~/.config/rofi
+rm -f ~/.config/rofi/config.rasi
 ln -s ~/Documents/GitHub/config-files/arch/rofi/config.rasi ~/.config/rofi/
 
 # picom
-mkdir ~/.config/picom
-rm ~/.config/picom/picom.conf
+mkdir -p ~/.config/picom
+rm -f ~/.config/picom/picom.conf
 ln -s ~/Documents/GitHub/config-files/arch/picom/picom.conf ~/.config/picom/
 
 # polybar
-mkdir ~/.config/polybar
-rm ~/.config/polybar/config.ini
+mkdir -p ~/.config/polybar
+rm -f ~/.config/polybar/config.ini
 ln -s ~/Documents/GitHub/config-files/arch/polybar/config.ini ~/.config/polybar/
-rm ~/.config/polybar/launch.sh
+rm -f ~/.config/polybar/launch.sh
 ln -s ~/Documents/GitHub/config-files/arch/polybar/launch.sh ~/.config/polybar/
 

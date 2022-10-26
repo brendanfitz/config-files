a6649740a9102298e8c30932e3687a6940f16a23
brendanfitz <brendanjfitzpatrick0@gmail.com>
2022-10-26 16:55:04 -0500

    Update i3 startup to basic config

--- arch/i3/i3_startup.sh
+++ arch/i3/i3_startup.sh
@@ -1,12 +1,16 @@
 i3-msg '[workspace=".*"] kill'
 
-i3-msg 'workspace 2; exec /usr/bin/jupyter notebook'
-sleep 1.5
-i3-msg 'workspace 1; exec /usr/bin/alacritty'
-sleep 1
-i3-msg 'workspace 1; exec /usr/bin/firefox --new-window https://www.wsj.com'
-sleep 1
-i3-msg 'workspace 3; exec /usr/bin/firefox --new-window https://www.gmail.com'
-sleep 1
-i3-msg 'workspace number 1'
+# i3-msg 'workspace 1; exec /usr/bin/jupyter notebook --no-browser'
 
+i3-msg 'workspace 1; exec /usr/bin/alacritty -e htop'
+i3-msg 'workspace 1; exec /usr/bin/alacritty -e nvtop'
+sleep 1
+
+i3-msg 'workspace 2; exec /usr/bin/alacritty -e neofetch'
+i3-msg 'workspace 2; exec /usr/bin/firefox --new-window'
+sleep 1
+
+# i3-msg 'workspace 3; exec /usr/bin/firefox --new-window 127.0.0.1:8000'
+# sleep 1
+
+i3-msg 'workspace number 2'

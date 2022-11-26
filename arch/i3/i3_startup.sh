feb81a9c1dd0f368f574317e9c93e60c1c1cda6b
brendanfitz <brendanjfitzpatrick0@gmail.com>
2022-11-26 12:34:42 -0600

    Simplify i3 startup

--- arch/i3/i3_startup.sh
+++ arch/i3/i3_startup.sh
@@ -1,16 +1,19 @@
+sleep 1
 i3-msg '[workspace=".*"] kill'
 
-# i3-msg 'workspace 1; exec /usr/bin/jupyter notebook --no-browser'
+# i3-msg 'workspace 1; exec --no-startup-id /usr/bin/jupyter notebook --no-browser'
 
-i3-msg 'workspace 1; exec /usr/bin/alacritty -e htop'
-i3-msg 'workspace 1; exec /usr/bin/alacritty -e nvtop'
+i3-msg 'workspace 1; exec --no-startup-id /usr/bin/alacritty -e htop'
+sleep 0.1
+i3-msg 'workspace 1; exec --no-startup-id /usr/bin/alacritty -e nvtop'
 sleep 1
 
-i3-msg 'workspace 2; exec /usr/bin/alacritty -e neofetch'
-i3-msg 'workspace 2; exec /usr/bin/firefox --new-window'
+# this neofetch is currently not working
+# i3-msg 'workspace 2; exec --no-startup-id /usr/bin/alacritty -e neofetch'
+i3-msg 'workspace 2; exec --no-startup-id /usr/bin/firefox --new-window'
 sleep 1
 
-# i3-msg 'workspace 3; exec /usr/bin/firefox --new-window 127.0.0.1:8000'
+# i3-msg 'workspace 3; exec --no-startup-id /usr/bin/firefox --new-window 127.0.0.1:8000'
 # sleep 1
 
 i3-msg 'workspace number 2'
